931cc0b98e70ed577a367326ea1b7592e07bdd69
ibarboxxx <gabriel.tintori2@gmail.com>
2019-10-19 09:49:17 +0200

    Fix tests

--- shop.py
+++ shop.py
@@ -1,3 +1,10 @@
 class Shop:
-    def __init__(self,address,a,b,c,d,e,f):
+    def __init__(self,address,open_hour,close_at,min_price,max_price,walk_in_start_hour,f):
         self.address = address
+        self.open_hour = open_hour
+        self.close_hour = close_at
+        self.min_price = min_price
+        self.max_price = max_price
+        self.avg_price = (min_price + max_price)/2
+        self.walk_in_start_hour. = walk_in_start_hour
+        
